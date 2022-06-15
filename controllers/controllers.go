f2115f9b6c2f5be873ec2d8a0c69a6efda2699bd
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-16 00:23:05 +0100

    created a greeter function for testing my code are well arranged and structured

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/KingAnointing/go-project/responses"
+	"github.com/gin-gonic/gin"
+)
+
+func Greeter() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, responses.Response{Status: http.StatusOK, Message: "sucess", Data: map[string]interface{}{"data": "Welcome to my personal skill test on gin, monogoDB & golang CRUD API"}})
+	}
+}
