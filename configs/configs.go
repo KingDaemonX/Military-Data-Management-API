cad3a1545d2bcd384e5ab91b25675b7c9186e94e
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-16 01:56:11 +0100

    corrected client by inserting applyuri

--- configs/configs.go
+++ configs/configs.go
@@ -1,18 +1,25 @@
 package configs
 
 import (
+	"context"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func ConnectDB()  {
+func ConnectDB() {
 	// create a client
-	client,err := mongo.NewClient(options.Client())
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoURI()))
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
+	cbg,cancel := context.WithTimeout(context.Background(), time.Second * 10)
+	defer cancel()
+
+	// connect client
+
 }
