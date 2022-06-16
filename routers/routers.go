67eb8948ec24841ca5463336f4e40c682029cfcc
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-16 01:21:15 +0100

    created the router function and the route for the greeter function

--- routers/routers.go
+++ routers/routers.go
@@ -1,1 +1,14 @@
 package routers
+
+import (
+	"github.com/KingAnointing/go-project/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func Router()  {
+	router := gin.Default()
+
+	router.GET("/api/",controllers.Greeter())
+
+	router.Run(":8080")
+}
