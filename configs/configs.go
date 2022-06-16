4ac9b2af156276661ecac834db7aff9fe062dd5b
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-16 01:52:00 +0100

    created a function to connect db and first create a mongodb client

--- configs/configs.go
+++ configs/configs.go
@@ -1,1 +1,18 @@
 package configs
+
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB()  {
+	// create a client
+	client,err := mongo.NewClient(options.Client())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+}
