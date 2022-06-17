2d9a4d721b5c7be20a9ddc52f6a577eae0ad86d8
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-17 21:20:13 +0100

    created gin function that will create user and also created the context type for the function and defer if it needs to be cancelled

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,14 +1,25 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/KingAnointing/go-project/responses"
 	"github.com/gin-gonic/gin"
 )
 
+// greeter function
 func Greeter() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, responses.Response{Status: http.StatusOK, Message: "sucess", Data: map[string]interface{}{"data": "Welcome to my personal skill test on gin, monogoDB & golang CRUD API"}})
+	}
+}
+
+// create function
+func CreateSoldier() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		cbg, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
 	}
 }
