4a30980dcd7e5264d73a5d7e40bfb35f1e13e673
KingAnointing <adetoyeanointing15@gmail.com>
2022-06-17 22:35:40 +0100

    added route to get soldier profile by id

--- routers/routers.go
+++ routers/routers.go
@@ -5,11 +5,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Router()  {
+func Router() {
 	router := gin.Default()
 
-	router.GET("/api/",controllers.Greeter())
-	router.POST("/api/soldier",controllers.CreateSoldier())
+	router.GET("/api/", controllers.Greeter())
+	router.POST("/api/soldier", controllers.CreateSoldier())
+	router.GET("/api/soldier/:userId", controllers.GetOneSoldier())
 
 	router.Run(":8080")
 }
