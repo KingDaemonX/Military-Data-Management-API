2ce3b5d88d199158fa8dcf5f9b1d14252b5e0c38
KingAnointing <adetoyeanointing15@gmail.com>
2022-08-02 08:25:12 +0100

    added field required for authentication like email,password,token,refreshtoken,...

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -10,21 +12,25 @@ type Army struct {
 }
 
 type Soldier struct {
-	ID         primitive.ObjectID `json:"id,omitempty"`
-	FirstName  string             `json:"first_name" validate:"required,min=2,max=200"`
-	LastName   string             `json:"last_name" validate:"required,min=2,max=200"`
-	NickName   string             `json:"nick_name" validate:"required,min=2,max=200"`
-	ArmyNumber string             `json:"army_id" validate:"required,min=2,max=200"`
-	Age        uint               `json:"age" validate:"required"`
-	// YearOfRecruitment time.Time          `json:"year_recruited" validate:"required min=2,max=200"`
-	// Updated           time.Time          `json:"updated" validate:"required"`
-	Rank            string    `json:"rank" validate:"required"`
-	NextOfKin       string    `json:"next_of_kin" validate:"required,min=2,max=200"`
-	ResidentBarrack string    `json:"resident_barracks" validate:"required,min=2,max=200"`
-	Address         string    `json:"address" validate:"required,min=2,max=200"`
-	PlaceOfService  string    `json:"place_of_service" validate:"required,min=2,max=200"`
-	IsAssignedArm   bool      `json:"is_armed"`
-	Division        *Division `json:"division"`
+	ID                primitive.ObjectID `json:"id,omitempty"`
+	FirstName         string             `json:"first_name" validate:"required,min=2,max=200"`
+	LastName          string             `json:"last_name" validate:"required,min=2,max=200"`
+	NickName          string             `json:"nick_name" validate:"required,min=2,max=200"`
+	Email             string             `json:"email" validate:"required,email"`
+	Password          string             `json:"password" validate:"required,min=8"`
+	ArmyNumber        string             `json:"army_id" validate:"required,min=2,max=200"`
+	Age               uint               `json:"age" validate:"required"`
+	YearOfRecruitment time.Time          `json:"year_recruited" validate:"required min=2,max=200"`
+	Updated           time.Time          `json:"updated" validate:"required"`
+	Rank              string             `json:"rank" validate:"required"`
+	NextOfKin         string             `json:"next_of_kin" validate:"required,min=2,max=200"`
+	ResidentBarrack   string             `json:"resident_barracks" validate:"required,min=2,max=200"`
+	Address           string             `json:"address" validate:"required,min=2,max=200"`
+	PlaceOfService    string             `json:"place_of_service" validate:"required,min=2,max=200"`
+	IsAssignedArm     bool               `json:"is_armed"`
+	Division          *Division          `json:"division"`
+	Token             string             `json:"token"`
+	Refresh_Token     string             `json:"refresh_token"`
 }
 
 type Division struct {
