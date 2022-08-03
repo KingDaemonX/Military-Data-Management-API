cdbd58d3c8a65dd814740c97cd1dac5f792a3b69
KingAnointing <adetoyeanointing15@gmail.com>
2022-08-03 04:21:52 +0100

    added a pointer to most of the string in the models

--- models/models.go
+++ models/models.go
@@ -13,33 +13,33 @@ type Army struct {
 
 type Soldier struct {
 	ID                  primitive.ObjectID `bson:"_id"`
-	User_id             string             `json:"user_id"`
-	First_name          string             `json:"first_name" validate:"required,min=2,max=200"`
-	Last_name           string             `json:"last_name" validate:"required,min=2,max=200"`
-	Nick_name           string             `json:"nick_name" validate:"required,min=2,max=200"`
-	Email               string             `json:"email" validate:"required,email"`
-	Password            string             `json:"password" validate:"required,min=8"`
-	Phone               string             `json:"phone" validate:"required"`
-	User_type           string             `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
-	Army_number         string             `json:"army_id" validate:"required,min=2,max=200"`
-	Age                 uint               `json:"age" validate:"required"`
+	User_id             string            `json:"user_id"`
+	First_name          *string            `json:"first_name" validate:"required,min=2,max=200"`
+	Last_name           *string            `json:"last_name" validate:"required,min=2,max=200"`
+	Nick_name           *string            `json:"nick_name" validate:"required,min=2,max=200"`
+	Email               *string            `json:"email" validate:"required,email"`
+	Password            *string            `json:"password" validate:"required,min=8"`
+	Phone               *string            `json:"phone" validate:"required"`
+	User_type           *string             `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	Army_number         *string            `json:"army_id" validate:"required,min=2,max=200"`
+	Age                 *uint              `json:"age" validate:"required"`
 	Year_of_recruitment time.Time          `json:"year_recruited" validate:"required min=2,max=200"`
 	Updated_at          time.Time          `json:"updated" validate:"required"`
-	Rank                string             `json:"rank" validate:"required"`
-	Next_of_kin         string             `json:"next_of_kin" validate:"required,min=2,max=200"`
-	Resident_barrack    string             `json:"resident_barracks" validate:"required,min=2,max=200"`
-	Address             string             `json:"address" validate:"required,min=2,max=200"`
-	Place_of_service    string             `json:"place_of_service" validate:"required,min=2,max=200"`
+	Rank                *string            `json:"rank" validate:"required"`
+	Next_of_kin         *string            `json:"next_of_kin" validate:"required,min=2,max=200"`
+	Resident_barrack    *string            `json:"resident_barracks" validate:"required,min=2,max=200"`
+	Address             *string            `json:"address" validate:"required,min=2,max=200"`
+	Place_of_service    *string            `json:"place_of_service" validate:"required,min=2,max=200"`
 	Is_assigned_arm     bool               `json:"is_armed"`
 	Division            *Division          `json:"division"`
-	Token               string             `json:"token"`
-	Refresh_Token       string             `json:"refresh_token"`
+	Token               *string            `json:"token"`
+	Refresh_Token       *string            `json:"refresh_token"`
 }
 
 type Division struct {
-	Division_name  string `json:"division_name" validate:"required,min=2,max=200"`
-	Commander_name string `json:"commander" validate:"required,min=2,max=200"`
-	Location       string `json:"location" validate:"required,min=2,max=200"`
-	Position       string `json:"position" validate:"required,min=2,max=200"`
-	Department     string `json:"department" validate:"required,min=2,max=200"`
+	Division_name  *string `json:"division_name" validate:"required,min=2,max=200"`
+	Commander_name *string `json:"commander" validate:"required,min=2,max=200"`
+	Location       *string `json:"location" validate:"required,min=2,max=200"`
+	Position       *string `json:"position" validate:"required,min=2,max=200"`
+	Department     *string `json:"department" validate:"required,min=2,max=200"`
 }
